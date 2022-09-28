664da4638e54e1fdef970610008b92050cbbe7f4
michaeljacob <Michael.Jacob@HMCTS.NET>
2022-09-28 12:55:20 +0100

    Remove component scan

--- src/main/java/uk/gov/hmcts/reform/em/ped/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/ped/Application.java
@@ -2,10 +2,8 @@ package uk.gov.hmcts.reform.em.ped;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "uk.gov.hmcts.reform.em.ped.")
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
